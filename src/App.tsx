ab1a5348ca29462a0ce63e04ac5e7990b55b0af5
Madhav Manohar S <madhavmanohar.s@gmail.com>
2022-07-20 00:55:21 +0100

    Replaced in-line loader and error messages
    Replaced in-line loader and error messages with components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
 import './App.css';
-import PriceChart from './components/PriceChart'
+import PriceChart from './components/charts/PriceChart'
 import fetchData from './utils/fetchData';
 import { useQuery } from "react-query";
+import Loader from './components/loader/Loader';
+import Error from './components/error/Error';
 
 
 function App() {
 
-  const {data, status ,error}:any = useQuery("prices", () => fetchData('data.json'));
-  
+  const {data, status ,error}:any = useQuery("prices", () => fetchData('dat.json'));
+  console.log(status,data,error);
   return (
     <div className="App">
       {status === 'loading' && (
-            <h1>Loading....</h1>
+            <Loader/>
         )}
       {status === 'success' && (
           <PriceChart {...{data, status ,error}}/>
+          
         )}
       {status === 'error' && (
-            <h1>{error.message}</h1>
+            <Error/>
         )} 
     </div>
   );
