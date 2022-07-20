fbead23679fb511d5c1d070359e2a4e9f7e257b5
Madhav Manohar S <madhavmanohar.s@gmail.com>
2022-07-20 01:09:01 +0100

    Changed http fetch to axios

--- src/utils/fetchData.ts
+++ src/utils/fetchData.ts
@@ -1,7 +1,13 @@
+import axios from "axios";
+
 const fetchData = async (path:string) => {
-    const response = await fetch(path).then(response => response.json()).
-    catch(err => console.log(err));
-    return response;
+    const {data}:any = await axios.get(path).then(response => response)
+    .catch(error => {
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+    })
+    return data;
 }
 
 export default fetchData;
