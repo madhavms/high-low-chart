a040d195b834d7aefd390d438d38d217a95a5d9f
Madhav Manohar S <madhavmanohar.s@gmail.com>
2022-08-30 14:26:42 +0530

    Update App.test.tsx
    Added test cases

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,53 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+import App from "./App";
+
+let mockStatus = "";
+let mockData: Array<string> = [];
+
+jest.mock("react-query", () => {
+  return {
+    __esModule: true,
+    useQuery: () => ({
+      data: mockData,
+      status: mockStatus,
+    }),
+  };
+});
+
+jest.mock("./chart", () => {
+  return {
+    __esModule: true,
+    Chart: () => <div>Chart Rendered</div>,
+  };
+});
+
+beforeEach(() => {
+  jest.resetModules();
+});
+
+describe("Chart-UI", () => {
+  test("renders Loading screen", () => {
+    mockStatus = "loading";
+    render(<App />);
+    const loaderElement = screen.getByText(/Loading/i);
+    expect(loaderElement).toBeInTheDocument();
+  });
+
+  test("renders error screen", () => {
+    mockStatus = "error";
+    render(<App />);
+    const errorElement = screen.getByText(/Error/i);
+    expect(errorElement).toBeInTheDocument();
+  });
+
+  test("renders chart", () => {
+    mockStatus = "";
+    mockData = ["chart Data"];
+    render(<App />);
+    const chartElement = screen.getByText(/Chart Rendered/i);
+    expect(chartElement).toBeInTheDocument();
+  });
 });
