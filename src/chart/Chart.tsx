0ca19854bafa41b0ba6a627e50b8786677c88bc1
Madhav Manohar S <madhavmanohar.s@gmail.com>
2022-08-30 14:25:36 +0530

    Create Chart.tsx
    Chart Component

---
+++ src/chart/Chart.tsx
@@ -0,0 +1,15 @@
+import Highcharts from "highcharts/highstock";
+import HighchartsReact from 'highcharts-react-official';
+import getOptions from '../utils/getOptions';
+
+export const Chart = ({data} :any) => {
+
+ return(
+    <HighchartsReact
+        constructorType={"stockChart"}
+        highcharts={Highcharts}
+        options={getOptions(data)}
+    />
+    )
+}
+
