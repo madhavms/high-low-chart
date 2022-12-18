c5af6b22869df5e1bd117d867a7110cd28a2f7a6
Madhav Manohar S <madhavmanohar.s@gmail.com>
2022-12-18 07:35:50 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css';
-import {Chart} from './chart/Chart'
 import fetchData from './utils/fetchData';
 import { useQuery } from "react-query";
 import Loader from './loader/Loader';
 import {Error} from './error/Error';
+import { lazy, Suspense } from 'react';
+
+const Chart = lazy(() => import('./chart/Chart'))
 
 
 function App() {
@@ -17,13 +19,13 @@ function App() {
   if (status === "error") {
     return <Error />;
   }
-
+  console.log('My status = ',status)
   return (
     <div className="App"> 
     <h1 className="text-3xl bg-blue-400 px-1 py-1">
         SPY Financial Data Chart
       </h1>
-    {data && data.length && <Chart data = {data}/>}
+    {data && data.length && <Suspense fallback={<Loader/>}><Chart data = {data}/></Suspense>}
     </div>
   );
 }
